ffb7e0087f0bae03ff40560dd89a693ea7aeb5d3
Yurii Habrusiev <yuriihabrusiev@users.noreply.github.com>
2024-03-26 12:02:18 +0000

    Add HostawayClient

---
+++ lib/hostaway_client.rb
@@ -0,0 +1,40 @@
+require 'singleton'
+
+class HostawayClient
+  include Singleton
+  include HTTParty
+
+  BASE_URL = "https://api.hostaway.com/v1".freeze
+  SUCCESS_STATUS = "success".freeze
+
+  def fetch_reservations
+    response = self.class.get(
+      "#{BASE_URL}/reservations",
+      headers: default_headers
+    )
+    if response["status"] == SUCCESS_STATUS
+      response["result"]
+    else
+      raise "Error fetching reservations"
+    end
+  end
+
+  private
+
+  def authenticate
+    self.class.post("#{BASE_URL}/accessTokens", body: {
+      client_id: Rails.application.credentials.hostaway.client_id,
+      client_secret: Rails.application.credentials.hostaway.client_secret,
+      grant_type: :client_credentials,
+      scope: :general
+    })
+  end
+
+  def access_token
+    @access_token ||= authenticate["access_token"]
+  end
+
+  def default_headers
+    @default_headers ||= { "Authorization" => "Bearer #{access_token}" }
+  end
+end
